b4527136f98e1a82d453256442b3733cb894132a
Pedro Nauck <pedronauck@gmail.com>
2019-02-26 19:19:51 -0300

    simplify project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import { render } from 'react-dom'
+import Hello from './Hello'
+import './style.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  const [name] = useState('World')
+  return (
+    <div>
+      <Hello name={name} />
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(<App />, document.getElementById('root'))
